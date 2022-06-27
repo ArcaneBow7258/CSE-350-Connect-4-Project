3927a14e330c8445c7400e774134a827fa98fece
Renzjordan1 <38296706+Renzjordan1@users.noreply.github.com>
2022-06-27 13:45:35 -0400

    Add files via upload
    Created barebones player class. Still needs min/max algorithms.

---
+++ Player.py
@@ -0,0 +1,30 @@
+
+import random
+
+class Player:
+
+    def __init__(self, isBot, color, difficulty=0):
+        #color input should be a variable in the format of (###,###,###)
+        self.isBot = isBot
+        self.color = color
+        self.difficulty = difficulty
+        
+
+    def move(self):
+        if(self.isBot):
+             match self.difficulty:
+                #Potentially pass in a list of available columns to optimize
+                case 1:
+                    col = random.randrange(1,7)
+                    return col
+                case 2:
+                    #Will use min/max algo
+                    return 0
+                case 3:
+                    #will use min/max algo
+                    return 0
+        
+        else:
+            #This function probably changes for human when we have a GUI (based on position on screen)
+            col = input("Enter column to place: ")
+            return col
